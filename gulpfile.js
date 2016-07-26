b63cac4428ca877a3a5bceb084b9b2fe8b66c597
sunny20131314 <https://sunny20131314@github.com>
2016-07-26 17:55:21 +0800

    file operate

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ gulp配置文件  add by sunzhimin
+ */
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+const babel = require('gulp-babel');
+var sourcemaps = require("gulp-sourcemaps");
+//var concat = require("gulp-concat");
+
+var jsUrl = 'lib/*.js';   //js 文件路径
+var jsPut = 'dist';  //js  压缩后文件路径
+
+
+gulp.task('es6', function() {
+  gulp.src( jsUrl )
+    .pipe(watch( jsUrl ))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    //.pipe(concat('all.js'))
+    //.pipe(sourcemaps.write("."))
+    .pipe(gulp.dest( jsPut ));
+});
+
+gulp.task('default', [ 'es6']);
+//gulp.task('default', ['less2css', 'post-css','minify-css', 'compress']);
